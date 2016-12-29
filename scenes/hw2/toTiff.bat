a170429f19293ed80fb04ae789ee511a18036eb7
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2016-12-29 12:45:18 +0800

    remove all cached scenes

--- scenes/hw2/toTiff.bat
+++
@@ -1,4 +0,0 @@
-..\..\bin\exrtotiff.exe dof-dragons.dgauss.exr dof-dragons.dgauss.tiff
-..\..\bin\exrtotiff.exe dof-dragons.fisheye.exr dof-dragons.fisheye.tiff
-..\..\bin\exrtotiff.exe dof-dragons.telephoto.exr dof-dragons.telephoto.tiff
-..\..\bin\exrtotiff.exe dof-dragons.wide.exr dof-dragons.wide.tiff
