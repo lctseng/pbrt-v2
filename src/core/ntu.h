111239873a4ae75dc58c47bb16dc1f7107b84e51
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-01-08 01:02:24 +0800

    change batch size, update display of timing

--- src/core/ntu.h
+++ src/core/ntu.h
@@ -7,14 +7,14 @@
 #if MEASURE_TIMING
 #define BEGIN_TIMING(FUNCTION_NAME) \
 	static long long total_time = 0.0; \
-	printf("\n" #FUNCTION_NAME "started\n"); \
+	printf("\n" #FUNCTION_NAME " started\n"); \
 	auto start = std::chrono::high_resolution_clock::now(); \
 
 #define END_TIMING(FUNCTION_NAME) \
 	auto delta = std::chrono::high_resolution_clock::now() - start; \
 	auto time = std::chrono::duration_cast<std::chrono::milliseconds>(delta).count(); \
 	total_time += time; \
-	printf("\n" #FUNCTION_NAME "used: %lld ms (total %lld ms)\n", time, total_time);
+	printf("\n" #FUNCTION_NAME " used: %lld ms (total %lld ms)\n", time, total_time);
 
 #else
 #define BEGIN_TIMING(FUNTION_NAME)
--- src/renderers/batchsamplerrenderer.h
+++ src/renderers/batchsamplerrenderer.h
@@ -95,7 +95,7 @@ private:
 };
 
 
-#define BATCH_RENDER_SIZE 10000
+#define BATCH_RENDER_SIZE 50000
 
 class BatchSamplerRendererQueue {
 public:
